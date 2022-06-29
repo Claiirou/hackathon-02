30c9b005d62325f235a9afe427ea2692fa081bac
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-06-29 19:15:52 +0200

    page creation

---
+++ pages/ProfileCreation.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function ProfileCreation() {
+  return (
+    <div>
+      <Layout pageTitle="Création de profile"></Layout>
+    </div>
+  );
+}
---
+++ pages/Projetcts.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function Projects() {
+  return (
+    <div>
+      <Layout pageTitle="Mes projets"></Layout>
+    </div>
+  );
+}
