7bef4eee70fa1daae61c9e10998c52e2365639c2
JVRasa <j.rasamoelina@gmail.com>
2022-06-29 09:43:53 +0200

    init next connect, prettier, eslint

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import base from "../../middlewares/common";
+
+function handleGet(req, res) {
+  res.status(200).json({ name: "John Doe" });
 }
+
+function handlePatch(req, res) {
+  res.status(201).json({ name: "John Doe" });
+}
+
+function handlePost(req, res) {
+  res.status(201).json({ name: "John Doe" });
+}
+
+export default base().get(handleGet).post(handlePost).patch(handlePatch);
