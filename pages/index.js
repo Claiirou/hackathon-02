38d24c6e85213fa7c0f7197abffa49887f8e0d31
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-06-30 15:23:42 +0200

    mapping on concepts

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import s from "../styles/Home.module.css";
 
 export default function Home() {
+  const [conceptsList, setConceptsList] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/api/concept")
+      .then((res) => res.data)
+      .then((data) => {
+        setConceptsList(data);
+      });
+  }, []);
+
+  console.log(conceptsList);
   return (
     <Layout pageTitle="Accueil du super hackathon">
       <div className={s.container}>
@@ -18,12 +31,20 @@ export default function Home() {
           </div>
           <div className={s.card}>
             <h2 className={s.card_title}>Mes Idées</h2>
-            <p className={s.card_content}>Nombre d'idées: </p>
-            <p className={s.card_content}>Nombre de likes: </p>
+            <p className={s.card_content}>
+              Nombre de d'idées: {conceptsList.length}{" "}
+            </p>
+            <ul>
+              {conceptsList.map((pro) => (
+                <li className={s.card_content} key={pro.id}>
+                  "{pro.name}"
+                </li>
+              ))}
+            </ul>
           </div>
           <div className={s.card}>
             <h2 className={s.card_title}>Mes Projets</h2>
-            <p className={s.card_content}>Nombre de projets: </p>
+            <p className={s.card_content}>Nombre de likes: </p>
           </div>
         </div>
       </div>
