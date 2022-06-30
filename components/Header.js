8124d3790439d9b066c7bc09d82fba77b13e0a6d
GaspardMzt <gaspard.mzt@gmail.com>
2022-06-30 11:31:02 +0200

    style done, need know to get the firstname from database

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,33 @@
 import React from "react";
 import Link from "next/link";
-// import Image from "next/image";
-import styles from "../styles/Header.module.css";
+import { CgProfile } from "react-icons/cg";
+import { MdNotificationsActive } from "react-icons/md";
+import { MdOutlineMessage } from "react-icons/md";
 
 export default function Header() {
   return (
-    <header className="bg-light-orange h-16 flex align-baseline">
-      <div className={styles.titleHeader}>
+    <header className="bg-light-orange h-16 text-white flex items-center justify-between">
+      <div className="ml-28  md:ml-48">
         <Link href="/">
-          <a>Apseedating</a>
+          <a className="font-['Montserrat'] text-xl font-bold">Apseedating</a>
         </Link>
       </div>
-      <div className={styles.profileContainer}></div>
+      <div className="mr-8 flex items-center ">
+        <MdNotificationsActive
+          style={{ width: "30px", height: "30px", margin: "5px" }}
+        />
+        <MdOutlineMessage
+          style={{ width: "30px", height: "30px", margin: "5px" }}
+        />
+        <CgProfile
+          style={{
+            width: "30px",
+            height: "30px",
+            margin: "auto 5px auto 15px",
+          }}
+        />
+        <p>Julie</p>
+      </div>
     </header>
   );
 }
