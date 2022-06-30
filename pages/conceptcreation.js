7e9ca981ab7de43b1d65787e5ecd6ea6e49480a2
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-06-30 17:49:49 +0200

    finished profile creation

--- pages/conceptcreation.js
+++ pages/conceptcreation.js
@@ -1,12 +1,41 @@
 import Layout from "../components/Layout";
 import { BsPlusLg } from "react-icons/bs";
+import { useRef, useState } from "react";
+import axios from "axios";
+import Image from "next/image";
 
 export default function ConceptCreation() {
+  const defaultState = {
+    name: "",
+    description: "",
+    domain: "",
+    status: "",
+    picture: "",
+    agency: "",
+  };
+  const [formInfos, setFormInfos] = useState(defaultState);
+
+  const photoUploadRef = useRef();
+
+  const handlePhotoClick = () => {
+    photoUploadRef.current.click();
+  };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formInfos);
+    try {
+      await axios.post("/api/concept", formInfos);
+      setFormInfos(defaultState);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <Layout pageTitle="Création d'un concept (idée/projet)">
       <div className="px-10 mt-6">
         <h1 className="text-4xl text-deep-orange">Création de concept :</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="flex justify-between align-top">
             <div className=" w-[50%]">
               <div className="flex flex-col my-2">
@@ -17,6 +46,10 @@ export default function ConceptCreation() {
                   type="text"
                   id="conceptName"
                   className="rounded-xl p-2 px-5"
+                  value={formInfos.name}
+                  onChange={(e) =>
+                    setFormInfos({ ...formInfos, name: e.target.value })
+                  }
                 />
               </div>
               <div className="flex flex-col my-2 py-2">
@@ -27,7 +60,12 @@ export default function ConceptCreation() {
                   name="concept"
                   id="status"
                   className="rounded-xl p-2 bg-white px-5 border-r-8 border-white"
+                  value={formInfos.status}
+                  onChange={(e) =>
+                    setFormInfos({ ...formInfos, status: e.target.value })
+                  }
                 >
+                  <option value="Choisir une option">Choisir une option</option>
                   <option value="Idée en cours">Idée en cours</option>
                   <option value="Idée validée">Idée validée</option>
                   <option value="Projet en cours">Projet en cours</option>
@@ -43,31 +81,60 @@ export default function ConceptCreation() {
                   type="text"
                   id="domain"
                   className="rounded-xl p-2 px-5"
+                  value={formInfos.domain}
+                  onChange={(e) =>
+                    setFormInfos({ ...formInfos, domain: e.target.value })
+                  }
                 />
               </div>
               <div className="flex flex-col my-2 py-2">
-                <label htmlFor="domain" className="py-1">
+                <label htmlFor="agency" className="py-1">
                   Agence :
                 </label>
                 <input
                   type="text"
-                  id="domain"
+                  id="agency"
                   className="rounded-xl p-2 px-5"
+                  value={formInfos.agency}
+                  onChange={(e) =>
+                    setFormInfos({ ...formInfos, agency: e.target.value })
+                  }
                 />
               </div>
             </div>
             <div className="my-4 py-2 px-3 w-[50%] flex justify-center flex-col items-center">
-              <p className="pb-2 self-start pl-[20%]">Photo :</p>
+              <label htmlFor="photo" className="pb-2 self-start pl-[20%]">
+                Photo :
+              </label>
               <div className="w-[60%] h-[300px] border-dashed border-2 border-deep-blue rounded-xl bg-dark-grey relative">
-                <div className="w-[100%] h-[100%] absolute top-0">
+                <input
+                  type="file"
+                  id="photo"
+                  accept="image/png, image/jpeg, image/jpg, image/gif"
+                  className="w-[100%] h-[100%] absolute top-0"
+                  value={formInfos.picture}
+                  onChange={(e) =>
+                    setFormInfos({ ...formInfos, picture: e.target.value })
+                  }
+                  ref={photoUploadRef}
+                  style={{ display: "none" }}
+                />
+                {formInfos.picture ? (
+                  <Image
+                    alt="photo"
+                    layout="fill"
+                    src={formInfos.picture.replace("C:\\fakepath\\", "/image/")}
+                  />
+                ) : (
                   <BsPlusLg
                     style={{
                       width: "30%",
                       height: "30%",
                       margin: "35% auto",
                     }}
+                    onClick={handlePhotoClick}
                   />
-                </div>
+                )}
               </div>
             </div>
           </div>
@@ -78,6 +145,10 @@ export default function ConceptCreation() {
             <textarea
               id="description"
               className="w-[100%] h-[100px] rounded-xl p-2 px-5"
+              value={formInfos.description}
+              onChange={(e) =>
+                setFormInfos({ ...formInfos, description: e.target.value })
+              }
             />
             <button
               type="submit"
